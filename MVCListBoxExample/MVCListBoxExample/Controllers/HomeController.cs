d3fa453a90b8dc55013a6ced90fdf5e987d84f14
geeksarray <36873556+geeksarray@users.noreply.github.com>
2020-09-24 08:55:11 +0530

    initial files

---
+++ MVCListBoxExample/MVCListBoxExample/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCListBoxExample.Models;
+
+namespace MVCListBoxExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetCategories()
+        {
+            Dictionary<string, string> lstCategories = new Dictionary<string, string>();
+            lstCategories.Add("1", "Beverages");
+            lstCategories.Add("2", "Condiments");
+            lstCategories.Add("3", "Confections");
+            lstCategories.Add("4", "Dairy Products");
+            lstCategories.Add("5", "Grains/Cereals");
+            lstCategories.Add("6", "Meat/Poultry");
+            lstCategories.Add("7", "Produce");
+            lstCategories.Add("8", "Seafood");            
+            return Json(lstCategories, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetCategoriesWithModel()
+        {
+            List<Category> lstCateogories = new List<Category>();
+            lstCateogories.Add(new Category() { CategoryID = "1", CategoryName = "Beverages" });
+            lstCateogories.Add(new Category() { CategoryID = "2", CategoryName = "Condiments" });
+            lstCateogories.Add(new Category() { CategoryID = "3", CategoryName = "Confections" });
+            lstCateogories.Add(new Category() { CategoryID = "4", CategoryName = "Dairy Products" });
+            lstCateogories.Add(new Category() { CategoryID = "5", CategoryName = "Grains/Cereals" });
+            lstCateogories.Add(new Category() { CategoryID = "6", CategoryName = "Meat/Poultry" });
+            lstCateogories.Add(new Category() { CategoryID = "7", CategoryName = "Produce" });
+            lstCateogories.Add(new Category() { CategoryID = "8", CategoryName = "Seafood" });
+
+            return Json(lstCateogories, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCListBoxExample/MVCListBoxExample/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCListBoxExample.Models
+{
+    public class Category
+    {
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
